2e4005aed273e3e34ca646aac7117e2ef284ce69
akudnamor <akudnamor@gmail.com>
2024-09-24 16:07:16 +0300

    init

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	fmt.Fprint(w, "Hello world!\n")
+	return
+}
+
+func main() {
+	router := httprouter.New()
+	router.GET("/", Index)
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
